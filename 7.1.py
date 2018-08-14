89fc260fd6c637cbbb50d0ba395b973c2cbf26c5
Sammy Carlson <carlson.samira@gmail.com>
2018-08-14 14:44:59 -0500

    Update 7.1.py

--- 7.1.py
+++ 7.1.py
@@ -1,5 +1,5 @@
-Write a program that prompts for a file name, then opens that file and reads through the file, 
-and print the contents of the file in upper case. Use the file words.txt to produce the output below.
+#Write a program that prompts for a file name, then opens that file and reads through the file, 
+#and print the contents of the file in upper case. Use the file words.txt to produce the output below.
 
 # Use words.txt as the file name
 fname = input("Enter file name: ")
