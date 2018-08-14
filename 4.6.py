e789ba6e8b78b39e313127c191ee4f0442dc36fe
Sammy Carlson <carlson.samira@gmail.com>
2018-08-14 14:45:43 -0500

    Update 4.6.py

--- 4.6.py
+++ 4.6.py
@@ -1,6 +1,6 @@
-Write a program to prompt the user for hours and rate per hour using input to compute gross pay. 
-Award time-and-a-half for the hourly rate for all hours worked above 40 hours. 
-Put the logic to do the computation of time-and-a-half in a function called computepay() and use the function to do the computation.
+#Write a program to prompt the user for hours and rate per hour using input to compute gross pay. 
+#Award time-and-a-half for the hourly rate for all hours worked above 40 hours. 
+#Put the logic to do the computation of time-and-a-half in a function called computepay() and use the function to do the computation.
 
 def computepay(h,r):
     if h > 40:
